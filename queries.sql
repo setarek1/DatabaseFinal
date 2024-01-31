2071cea5d35cd15915c0f88b5bb0ce1aebd80b1a
setarek1 <101448777+setarek1@users.noreply.github.com>
2024-01-31 11:34:53 +0330

    harder2 complete

--- queries.sql
+++ queries.sql
@@ -5,54 +5,45 @@
 -- INSERT INTO currencyMarket(currencyID, name, buyRate, sellRate) VALUES(4, 'euro', 10, 10);
 -- DROP TABLE currencymarket;
 -- SELECT * FROM currencymarket;
--- easy: DONE
--- SELECT 
--- 	userID, 
---     userName, 
---     dollarBalance, tomanBalance, lireBalance, poundBalance, euroBalance 
--- FROM wallet w, projschema.user u
--- WHERE u.userID = w.user_userID;
+-- easy
+SELECT 
+	userID, 
+    userName, 
+    dollarBalance, tomanBalance, lireBalance, poundBalance, euroBalance 
+FROM wallet w, projschema.user u
+WHERE u.userID = w.user_userID;
 
--- hard: DONE
--- SELECT 
--- 	transactionID,
---     u1.userName AS Seller,
---     u2.userName AS Buyer,
---     c1.name AS BaseCurrency,
---     c2.name AS TargetCurrency
--- FROM 
--- 	transaction t 
--- JOIN 
---     user u1 ON t.user_sellerID = u1.userID
--- JOIN 
--- 	user u2 ON t.user_buyerID1 - u2.userID
--- JOIN 
--- 	currencyMarket c1 ON c1.currencyID = t.currency_fromID
--- JOIN
--- 	currencyMarket c2 ON c2.currencyID = t.currency_toID
--- WHERE
--- 	(t.date >'2009-01-01' AND t.date <'2010-01-01') AND 
---     ((c1.name = 'toman' AND c2.name = 'dollar') OR (c1.name = 'dollar' AND c2.name = 'toman'));
+-- hard
+SELECT 
+	transactionID,
+    u1.userName AS Seller,
+    u2.userName AS Buyer,
+    c1.name AS BaseCurrency,
+    c2.name AS TargetCurrency
+FROM 
+	transaction t 
+JOIN 
+    user u1 ON t.user_sellerID = u1.userID
+JOIN 
+	user u2 ON t.user_buyerID1 - u2.userID
+JOIN 
+	currencyMarket c1 ON c1.currencyID = t.currency_fromID
+JOIN
+	currencyMarket c2 ON c2.currencyID = t.currency_toID
+WHERE
+	(t.date >'2009-01-01' AND t.date <'2010-01-01') AND 
+    ((c1.name = 'toman' AND c2.name = 'dollar') OR (c1.name = 'dollar' AND c2.name = 'toman'));
 
 -- harder 1
--- SELECT m.name AS currency, 
--- 	EXTRACT(MONTH FROM t.date) AS monthNumber, 
---     (SELECT SUM(amount) FROM (SELECT user_buyerID1, amount FROM transaction t2 WHERE t2.currency_fromID=m.currencyID)) AS buy,  
---     (SELECT SUM(amount2)FROM (SELECT user_sellerID, amount2 FROM transaction t3 WHERE t3.currrency_toID=m.currencyID)) AS sell
--- FROM transaction t, currencyMarket m
--- WHERE EXTRACT(YEAR FROM t.date) = '2009'
--- -- total = SELECT IF(m.currencyID = t.currency_fromID, t.amount, amount2) FROM transaction
--- GROUP BY currency, monthNumber
--- ORDER by currency
--- ;
-
 SELECT currency, monthNumber, buy, sell, buy+sell AS total FROM
 (
  SELECT
     m.name AS currency,
     EXTRACT(MONTH FROM t.date) AS monthNumber,
     COALESCE(SUM(t2.amount), 0) AS buy,
-    COALESCE(SUM(t3.amount2), 0) AS sell
+    COALESCE(SUM(t3.amount2), 0) AS sell,
+    COUNT(t2.amount) AS c1,
+    COUNT(t3.amount2) AS c2
 FROM
     currencyMarket m
 JOIN
@@ -71,5 +62,32 @@ GROUP BY
 ORDER BY
     currency, monthNumber;
 
+
+
 -- harder 2
+SELECT
+    userID,
+    userName,
+    a1 + a2 AS `total in dollars`
+FROM
+    (
+        SELECT
+            u.userID,
+            u.userName,
+            SUM(t1.amount) AS a1,
+            SUM(t2.amount2) AS a2
+        FROM
+            user u
+        JOIN
+            (SELECT * FROM transaction WHERE currency_fromID = 0 AND currency_toID = 1) AS t1
+            ON (u.userID = t1.user_buyerID1 OR u.userID = t1.user_sellerID)
+        INNER JOIN
+            (SELECT * FROM transaction WHERE currency_fromID = 1 AND currency_toID = 0) AS t2
+            ON (u.userID = t2.user_buyerID1 OR u.userID = t2.user_sellerID)
+        GROUP BY
+            u.userID, u.userName
+    ) AS returned
+ORDER BY
+    `total in dollars` DESC
+LIMIT 5;
 
